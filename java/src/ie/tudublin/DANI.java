ac9d390f152eff664ae8312bbdfaac008f068664
elicookies <elikoleva32@gmail.com>
2023-04-26 17:13:12 +0100

    skeleton done, loadFile() almost done

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -6,6 +6,31 @@ import processing.core.PApplet;
 
 public class DANI extends PApplet {
 
+
+	class Word {
+		private String word;
+		private ArrayList<Follow> follows;
+	
+		public Word(String word) {
+			this.word = word;
+			this.follows = new ArrayList<>();
+		}
+
+		public String getWord() {
+			return word;
+		}
+
+		public ArrayList<Follow> getFollows() {
+			return follows;
+		}
+
+		@Override
+		public String toString() {
+			for(Follow f:follows){
+				System.out.println(f.getWord() + ": " + f + "(" + f.getCount()+ ")");
+		}
+	
+	}
 	
 	class Follow {
 		private String word;
@@ -26,34 +51,65 @@ public class DANI extends PApplet {
 
 		@Override
 		public String toString() {
-			return "Follow [count=" + count + ", word=" + word + "]";
+			return word + "(" + count + ")";
 		}
 	}
 	
-	class Word {
-		private String word;
-		private ArrayList<Follow> follows;
-	
-		public Word(String word) {
-			this.word = word;
-			this.follows = new ArrayList<>();
+
+	void loadFile(){
+		String[] lines = loadStrings("small.txt");
+		for(String line:lines){
+			String[] words = split(line, ' ');
+			Word[] Words = new Word[words.length];
+
+			for(int i = 0 ; i < words.length - 1 ; i ++)
+			{
+				String word = words[i];
+				String next = words[i + 1];
+				//System.out.println(word + " -> " + next);
+				Word wd = findWord(word);
+				if(wd == null)
+				{
+					wd = new Word(word);
+					Words[i] = wd;
+					
+				}
+				Follow f = wd.findFollow(next);
+				if(f == null)
+				{
+					f = new Follow(next, 1);
+					wd.getFollows().add(f);
+				}
+				else
+				{
+					int f_count = f.getCount(); 
+					f.count = f_count + 1;
+				}
+			}
 		}
 
-		public String getWord() {
-			return word;
-		}
 
-		public ArrayList<Follow> getFollows() {
-			return follows;
-		}
-
-		@Override
-		public String toString() {
-			return "Word [follows=" + follows + ", word=" + word + "]";
-		}
-	
 	}
 	
+	//finds a word in the model so you can check if it already exists, returns null if no match
+	Word findWord(str){
+
+
+		return null;
+
+	}
+
+	Follow findFollow(str){
+
+
+		return null;
+
+	}
+
+	void printModel(){
+
+	}
+
 	public void settings() {
 		size(1000, 1000);
 		//fullScreen(SPAN);
@@ -62,18 +118,32 @@ public class DANI extends PApplet {
     String[] sonnet;
 
     public String[] writeSonnet()
-    {
+    {	
+		//pick random word
+
+		//if it has no follow word use it
+		// otherwise pick a follow word at random and use it
+		// repeat 8 times
+		//Print the Sonnet to the console
+		// Print the Sonnet to the screen.
+		// Add code to generate a new sonnet on keyPress
         return null;
     }
 
 	public void setup() {
 		colorMode(HSB);
+		loadFile();
+		//writeSonnet();
+
 
        
 	}
 
 	public void keyPressed() {
-
+		if (keyCode == ' ')
+		{
+			//writeSonnet();
+		}
 	}
 
 	float off = 0;
